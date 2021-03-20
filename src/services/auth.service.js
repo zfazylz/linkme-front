ef693a981179c872766ac96d7ba1ee9ef1ff79b7
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-03-20 18:32:45 +0600

    Send AITU data on auth

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 import {API_URL, isResponseOK} from "."
+import aituBridge from "@btsd/aitu-bridge";
 
 class AuthService {
   login(username, password) {
+    const aituData = aituBridge.getMe();
     return axios
       .post(
         API_URL + "auth/api/token/",
-        {email: username, password},
+        {email: username, password, aitu_data: aituData},
       )
       .then((response) => {
         if (isResponseOK(response)) {
