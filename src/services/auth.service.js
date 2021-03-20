52b5b582a4e17e2d060f7f8940d3ef0ffdce6ab6
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-03-20 19:48:39 +0600

    AITU Bridge fix

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 import {API_URL, isResponseOK} from "."
-import aituBridge from "@btsd/aitu-bridge";
 
 class AuthService {
-  login(username, password) {
-    const aituData = aituBridge.getMe();
+  login(username, password, aituData) {
     return axios
       .post(
         API_URL + "auth/api/token/",
