4c6fce5556214504b6390f39ba33b9092ed56959
Sequner <sanzha.shabdarov@gmail.com>
2021-03-20 23:58:32 +0600

    cardFinal

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,33 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
 import PersonIcon from "@material-ui/icons/Person";
 import ForumIcon from "@material-ui/icons/Forum";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import IconButton from "@material-ui/core/IconButton";
-import {Link, useHistory} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
-const Header = ({backButton}) => {
+const Header = ({ backButton }) => {
   const history = useHistory();
   return (
     <div className="header">
       {backButton ? (
         <IconButton onClick={() => history.replace(backButton)}>
-          <ArrowBackIosIcon className="header_icon" fontSize="large"/>
+          <ArrowBackIosIcon className="header_icon" fontSize="large" />
         </IconButton>
       ) : (
         <Link to="/profile">
           <IconButton>
-            <PersonIcon className="header_icon" fontSize="large"/>
+            <PersonIcon className="header_icon" fontSize="large" />
           </IconButton>
         </Link>
       )}
 
       <Link to="/">
-        <img
-          className="header_logo"
-          src={"/logo.png"}
-          alt="LinkMe App"
-        />
+        <img className="header_logo" src={"/logo.png"} alt="LinkMe App" />
       </Link>
       <Link to="/chat">
         <IconButton>
-          <ForumIcon className="header_icon" fontSize="large"/>
+          <ForumIcon className="header_icon" fontSize="large" />
         </IconButton>
       </Link>
     </div>
--- src/components/TinderCards/TinderCards.js
+++ src/components/TinderCards/TinderCards.js
@@ -1,28 +1,35 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
 import CardService from "../../services/card.service";
-import SwipeButtons from "../../components/SwipeButtons/SwipeButtons";
+import "./../SwipeButtons/SwipeButtons.css";
+
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 class TinderCards extends Component {
-  state = {cards: [], remainingCards: 0};
+  state = { cards: [], cardID: 0, newCards: [], refList: [] };
 
   componentDidMount() {
     this.loadCards(1);
+    this.setState({
+      refList: Array(10)
+        .fill(0)
+        .map((i) => React.createRef()),
+    });
   }
 
   evaluateCard = (uid, dir) => {
     let mark = dir === "right";
-
+    this.setState({
+      cardID: this.state.cardID - 1,
+    });
     CardService.evaluateCard(uid, mark).then(
       (response) => {
         console.log(response.data);
-        this.setState({
-            remainingCards: this.state.remainingCards - 1
-          }
-        );
-        if (this.state.remainingCards === 0) {
+        if (this.state.cardID === 0) {
           this.loadCards(1);
         }
       },
@@ -44,7 +51,7 @@ class TinderCards extends Component {
       (response) => {
         this.setState({
           cards: response.data,
-          remainingCards: response.data.results.length,
+          cardID: response.data.results.length - 1,
         });
       },
       (error) => {
@@ -60,19 +67,34 @@ class TinderCards extends Component {
     );
   };
 
+  swipeBut = (dir) => {
+    if (this.state.cardID >= 0) {
+      console.log("swipe" + this.state.cardID);
+      this.state.refList[this.state.cardID].current.swipe(dir); // Swipe the card!
+    }
+  };
+
   render() {
+    if (!this.state.cards.results?.length) {
+      return (
+        <div className="emptyCardContainer">
+          <h1>Мы в поиске новой пары для тебя не переживай!</h1>
+        </div>
+      );
+    }
     return (
       <div>
         <div className="tinderCards_cardContainer">
-          {this.state.cards?.results?.map((person) => (
+          {this.state.cards?.results?.map((person, index) => (
             <TinderCard
+              ref={this.state.refList[index]}
               onSwipe={(dir) => this.evaluateCard(person.user_id, dir)}
               className="swipe"
               key={person.user_id}
               preventSwipe={["up", "down"]}
             >
               <div
-                style={{backgroundImage: `url(${person.photos[0]})`}}
+                style={{ backgroundImage: `url(${person.photos[0]})` }}
                 className="card"
               >
                 <h3>{person.username}</h3>
@@ -80,14 +102,27 @@ class TinderCards extends Component {
             </TinderCard>
           ))}
         </div>
-        <SwipeButtons/>
+        <div className="swipeButtons">
+          <IconButton
+            onClick={() => this.swipeBut("left")}
+            className="swipeButtons_left"
+          >
+            <CloseIcon fontSize="large" />
+          </IconButton>
+          <IconButton
+            onClick={() => this.swipeBut("right")}
+            className="swipeButtons_right"
+          >
+            <FavoriteIcon fontSize="large" />
+          </IconButton>
+        </div>
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  const {user} = state.auth;
+  const { user } = state.auth;
   return {
     user,
   };
