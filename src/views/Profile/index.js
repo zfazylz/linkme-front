f0ddd35a607e4437ad28da4b4ba476cbd0fdd6a5
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-03-21 03:10:33 +0600

    Profile view filled

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,12 +1,54 @@
+import React, {Component} from "react";
+import {Redirect, Route} from 'react-router-dom';
+import ProfileService from "../../services/profile.service";
+import TinderCard from "react-tinder-card";
 import Header from "../../components/Header/Header";
-import React from "react";
 
-const ProfileView = () => {
+export default class ProfileView extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      content: null
+    };
+  }
+
+  componentDidMount() {
+    ProfileService.myProfile().then(
+      response => {
+        this.setState({
+          content: response.data
+        });
+      },
+      error => {
+        this.setState({
+          content:
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString()
+        });
+      }
+    );
+  }
+
+  render() {
+    const profile = this.state.content;
     return (
-        <div>
-            <Header backButton="/"/>
+      <div>
+        <Header/>
+        <div className="emptyCardContainer">
+          <h1>Профиль</h1>
+          <div
+            style={{backgroundImage: `url(${profile?.avatar_url})`}}
+            className="card"
+          >
+            <h3>{profile?.username}</h3>
+            <p>{profile?.description}</p>
+          </div>
         </div>
-    )
+      </div>
+    );
+  }
 }
-
-export default ProfileView;
