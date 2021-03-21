6620978a6cbb036a9a89e8ad3d1d8638240f0b2d
faza.zheten123@gmail.com <faza.zheten123@gmail.com>
2021-03-21 13:34:23 +0600

    Logout button to profile view

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,14 +1,23 @@
 import React, {Component} from "react";
 import ProfileService from "../../services/profile.service";
 import Header from "../../components/Header/Header";
+import {logout} from "../../actions/auth";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import {connect} from "react-redux";
+import {Link} from "react-router-dom";
 
-export default class ProfileView extends Component {
+class ProfileView extends Component {
   constructor(props) {
     super(props);
+    this.logOutLink = this.logOutLink.bind(this);
 
     this.state = {
       content: null
     };
+  }
+
+  logOutLink() {
+    this.props.dispatch(logout());
   }
 
   componentDidMount() {
@@ -46,7 +55,25 @@ export default class ProfileView extends Component {
             <p>{profile?.description}</p>
           </div>
         </div>
+        <div className="swipeButtons">
+          <Link
+            to=""
+            onClick={this.logOutLink}
+            className="swipeButtons_left"
+          >
+            <ExitToAppIcon fontSize="large"/>
+          </Link>
+        </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  const { user } = state.auth;
+  return {
+    user,
+  };
+}
+
+export default connect(mapStateToProps)(ProfileView);
